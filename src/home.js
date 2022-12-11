e72b82de49bee151160effbc9b92d9d0ce0879a3
rajeshthakuri <rajeshthakuri@gmail.com>
2022-12-11 22:32:51 +0530

    home componet create

---
+++ src/home.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Home1()
+{
+    //Signle element    
+    /*return(
+        <h1>Welcome to React Project</h1>
+    )*/
+    //Multiple Elements
+    return(
+        <>
+            <h1>Welcome to React Project</h1>
+            <h3>Using by JavaScript</h3>
+        </>
+    )
+}
+export default Home1
